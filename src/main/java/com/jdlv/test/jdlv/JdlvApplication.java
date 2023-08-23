55e6f5182218b76f9485d21cb356c05080b41214
Nicolas BENIZRI <20011494>
2023-08-23 17:28:02 +0200

    init

---
+++ src/main/java/com/jdlv/test/jdlv/JdlvApplication.java
@@ -0,0 +1,13 @@
+package com.jdlv.test.jdlv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdlvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdlvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdlv/test/jdlv/JdlvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdlv.test.jdlv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdlvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
